15cd454025550cb361d3b3c25973d8c29ceca95b
Luis Finke <lfinke@fusionalliance.com>
2020-02-01 14:00:02 -0500

    MagnetArm and Magnet

---
+++ Assets/Scripts/Magnet/Magnet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet: MonoBehaviour {
+	private List<GameObject> stuckObjects;
+
+	// Start is called before the first frame update
+	void Start() {
+		//
+	}
+
+	// Update is called once per frame
+	void Update() {
+		//
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if(collider.gameObject.tag.Equals("Magnetic")) {
+			stuckObjects.Add(collider.gameObject);
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		stuckObjects.Remove(collider.gameObject);
+	}
+}
---
+++ Assets/Scripts/Magnet/MagnetArm.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagnetArm: MonoBehaviour {
+	public ExpandableRod expandableRod;
+	public GameObject magnetPivot;
+	public float verticalMoveSpeed = 0.01f;
+	public float horizontalMoveSpeed = 0.01f;
+	public float rotationSpeed = 0.2f;
+
+	// Use this for initialization
+	void Start() {
+		//
+	}
+
+	// Update is called once per frame
+	void Update() {
+		//
+	}
+
+	void moveDown() {
+		expandableRod.expandRod(verticalMoveSpeed);
+	}
+
+	void moveUp() {
+		expandableRod.expandRod(-verticalMoveSpeed);
+	}
+
+	void rotateLeft() {
+		var rotation = magnetPivot.transform.localRotation;
+		rotation.z = rotation.z - rotationSpeed;
+		magnetPivot.transform.localRotation = rotation;
+	}
+
+	void rotateRight() {
+		var rotation = magnetPivot.transform.localRotation;
+		rotation.z = rotation.z + rotationSpeed;
+		magnetPivot.transform.localRotation = rotation;
+	}
+}
