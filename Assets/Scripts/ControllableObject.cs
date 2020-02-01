fc9ef5d7371fd86b43fc21a4cfcdedf5d3e79da5
Ryan-PC <promo@rydwy.com>
2020-02-01 12:14:11 -0500

    Separated controls

---
+++ Assets/Scripts/ControllableObject.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllableObject : MonoBehaviour
+{
+
+    private KeyCode up, down, left, right, jump;
+    void Start()
+    {
+        left = KeyCode.A;
+        right = KeyCode.D;
+        up = KeyCode.W;
+        down = KeyCode.S;
+        jump = KeyCode.Space;
+    }
+
+    private float moveHorizontal, falltimer;
+    public void Movement(Rigidbody2D myRigidbody)
+    {
+        if (Input.GetKeyDown(jump))
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, myRigidbody.velocity.y + 5.0f);
+        }
+        falltimer += Time.deltaTime * 12.5f;
+        if (Input.GetKey(left))
+        {
+            moveHorizontal = -1.5f;
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if (Input.GetKey(right))
+        {
+            moveHorizontal = 1.5f;
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+        if (!Input.GetKey(down))
+        {
+            falltimer = 0;
+            myRigidbody.velocity = new Vector2(moveHorizontal * 5.0f, myRigidbody.velocity.y);
+            transform.rotation = Quaternion.identity;
+        }
+        else if (transform.position.y > 0.0f && Input.GetKey(KeyCode.S))
+        {
+            gameObject.GetComponent<CircleCollider2D>().isTrigger = true;
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, -4.0f - falltimer);
+            transform.rotation = Quaternion.identity;
+        }
+
+        if (!Input.GetKey(down) && !Input.GetKey(right) && !Input.GetKey(left))
+        {
+            myRigidbody.velocity = new Vector2(0.0f, myRigidbody.velocity.y);
+            transform.rotation = Quaternion.identity;
+        }
+    }
+
+    public void climbLadder(Rigidbody2D myRigidbody)
+    {
+        //Debug.Log("Colliding!");
+        if (Input.GetKey(up)) //Move Up ladder
+        {
+            myRigidbody.velocity = new Vector2(0.0f, 4.0f);
+            gameObject.GetComponent<CircleCollider2D>().isTrigger = true;
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,101 +5,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public ControllableObject controls;
     public float walkSpeed = 5.0f;
     private Rigidbody2D myRigidbody;
     private float falltimer;
 
 
     //VARIABLES FOR SEPARATE CONTROLS FOR EACH PLAYER
-    private KeyCode left, right, up, down, jump;
 
     void Start()
     {
+        controls = gameObject.GetComponent<ControllableObject>();
         myRigidbody = gameObject.GetComponent<Rigidbody2D>();
-        Debug.Log(GameObject.Find("Players").transform.childCount);
-        
     }
 
     void FixedUpdate()
     {
-        left = KeyCode.A;
-        right = KeyCode.D;
-        up = KeyCode.W;
-        down = KeyCode.S;
-        jump = KeyCode.Space;
-        Movement();
+        controls.Movement(myRigidbody);
         transform.rotation = Quaternion.identity; //Stop player rotating
     }
 
-    private float moveHorizontal;
-    void Movement()
-    {
-
-        //Jumping
-        if (Input.GetKeyDown(jump))
-        {
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, myRigidbody.velocity.y + 5.0f);
-        }
-        falltimer += Time.deltaTime * 12.5f;
-        #region Player Movement
-
-        //TODO: Make players slow down/stop moving when not pressing any buttons
-        if (Input.GetKey(left))
-        {
-            moveHorizontal = -1.5f;
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        }
-
-        if (Input.GetKey(right))
-        {
-            moveHorizontal = 1.5f;
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-        }
-
-        if (!Input.GetKey(down))
-        {
-            falltimer = 0;
-            myRigidbody.velocity = new Vector2(moveHorizontal * walkSpeed, myRigidbody.velocity.y);
-            transform.rotation = Quaternion.identity;
-        }
-        else if (transform.position.y > 0.0f && Input.GetKey(KeyCode.S))
-        {
-            //Debug.Log("Timer: " + falltimer);
-            gameObject.GetComponent<CircleCollider2D>().isTrigger = true;
-            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, -4.0f - falltimer);
-            transform.rotation = Quaternion.identity;
-        }
-
-        if(!Input.GetKey(down) && !Input.GetKey(right) && !Input.GetKey(left))
-        {
-            myRigidbody.velocity = new Vector2(0.0f, myRigidbody.velocity.y);
-            transform.rotation = Quaternion.identity;
-        }
-        #endregion
-
-        
-    }
 
     void OnTriggerStay2D(Collider2D other)
     {
         //Ladder Collision
         if (other.gameObject.CompareTag("Ladder"))
         {
-            //Debug.Log("Colliding!");
-            if (Input.GetKey(up)) //Move Up ladder
-            {
-                myRigidbody.velocity = new Vector2(0.0f, 4.0f);
-                gameObject.GetComponent<CircleCollider2D>().isTrigger = true;
-                transform.rotation = Quaternion.identity;
-            }
+            controls.climbLadder(myRigidbody);
         }
     }
 
+
+
+    //----This stuff stops the player from falling thru platforms and ladders----
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Bottom"))
         {
-            //Debug.Log("Turning off trigger!");
             gameObject.GetComponent<CircleCollider2D>().isTrigger = false;
         }
     }
@@ -114,7 +56,6 @@ public class Player : MonoBehaviour
 
         if (other.gameObject.CompareTag("Platform") && myRigidbody.velocity.y < -2.0f)
         {
-            //Debug.Log("Turning off trigger!");
             gameObject.GetComponent<CircleCollider2D>().isTrigger = false;
         }
     }
@@ -123,7 +64,6 @@ public class Player : MonoBehaviour
     {
         if (coll.gameObject.CompareTag("Platform"))
         {
-            //Debug.Log("Turning off trigger!");
             gameObject.GetComponent<CircleCollider2D>().isTrigger = false;
         }
     }
