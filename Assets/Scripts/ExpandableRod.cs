b27d8457ae126cd75db2dbe1c82f2d28bf3804fc
Luis Finke <lfinke@fusionalliance.com>
2020-02-01 11:49:28 -0500

    ExpandableRod

---
+++ Assets/Scripts/ExpandableRod.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandableRod: MonoBehaviour {
+	public GameObject rodBody;
+	public float initialRodHeight = 2.0f;
+	private float rodHeight;
+
+	// Start is called before the first frame update
+	void Start() {
+		setRodHeight(initialRodHeight);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if(Input.GetKey(KeyCode.T)) {
+			float newRodHeight = rodHeight + 0.05f;
+			setRodHeight(newRodHeight);
+		} else if(Input.GetKey(KeyCode.G)) {
+			float newRodHeight = rodHeight - 0.05f;
+			if(newRodHeight < 0.2f) {
+				newRodHeight = 0.2f;
+			}
+			setRodHeight(newRodHeight);
+		}
+	}
+
+	void setRodHeight(float height) {
+		var spriteRenderer = rodBody.GetComponent<SpriteRenderer>();
+		var spriteSize = spriteRenderer.size;
+		spriteSize.y = height;
+		spriteRenderer.size = spriteSize;
+		rodBody.transform.localPosition = new Vector2(0, -height);
+		rodHeight = height;
+	}
+}
