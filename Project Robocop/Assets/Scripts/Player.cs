c4b997d2005f355664f8e27b717587bcf6445fc7
Ryan-Laptop <ryan_dwyer@outlook.com>
2020-01-31 21:04:55 -0500

    Implement player movement

--- Project Robocop/Assets/Scripts/Player.cs
+++ Project Robocop/Assets/Scripts/Player.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float walkSpeed = 5.0f;
+    private Rigidbody2D myRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        #region Player Movement
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetKey(KeyCode.W)) //Move Up ladder
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 4.0f);
+        }
+        else if (Input.GetKey(KeyCode.S)) //Move down ladder
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, -4.0f);
+        }
+
+
+        myRigidbody.velocity = new Vector2(moveHorizontal * walkSpeed, myRigidbody.velocity.y);
         
+
+
+
+        #endregion
     }
 }
