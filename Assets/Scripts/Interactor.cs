268dfbb8d21bfd50fc8fcf9c7677839dfb37c6b6
Luis Finke <lfinke@fusionalliance.com>
2020-02-01 02:20:26 -0500

    Interactor and Interactable

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactor: MonoBehaviour {
+	// Use this for initialization
+	void Start() {
+		//
+	}
+
+	// Update is called once per frame
+	void Update() {
+		//
+	}
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		var interactable = collision.gameObject.GetComponent<Interactable>();
+		if(interactable != null) {
+			interactable.beginInteractor(this);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D collision) {
+		var interactable = collision.gameObject.GetComponent<Interactable>();
+		if (interactable != null) {
+			interactable.endInteractor(this);
+		}
+	}
+}
