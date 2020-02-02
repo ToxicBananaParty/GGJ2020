48780b3980e4a55c4cbc56a8f23fb688eb990904
Luis Finke <lfinke@fusionalliance.com>
2020-02-01 20:06:51 -0500

    ScrapButtonInteraction

--- Assets/Scripts/Behaviors/StationInteraction.cs
+++ Assets/Scripts/Behaviors/StationInteraction.cs
@@ -25,15 +25,8 @@ public class StationInteraction : Interactable
 	}
 
 	public override void performAction(Interactor interactor) {
-	    if (!interactor.gameObject.name.Equals("Scrap Station") && !interactor.gameObject.name.Equals("Paint Station"))
-	    {
-	        var player = interactor.GetComponent<Player>();
-	        var stationControls = GetComponent<StationControls>();
-	        player.controls.attachStation(stationControls);
-	    }
-	    else //Do not change controls for paint or scrap stations, just press F to get scrap / paint bot
-	    {
-	        Instantiate(scrapPrefab, GameObject.Find("ScrapSpawn").transform);
-	    }
+	    var player = interactor.GetComponent<Player>();
+	    var stationControls = GetComponent<StationControls>();
+	    player.controls.attachStation(stationControls);
 	}
 }
