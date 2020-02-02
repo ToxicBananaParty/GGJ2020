8594b8d631a0e2a0f96588d084be5cd26179157d
Luis Finke <lfinke@fusionalliance.com>
2020-02-02 13:17:14 -0500

    Globals

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals: MonoBehaviour {
+	public static Globals shared;
+
+	// Use this for initialization
+	void Start() {
+		shared = this;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		//
+	}
+}
